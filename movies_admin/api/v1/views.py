31208ecb168376807981234c0c2159310ae4728b
Viktor.Vasilev <Viktor.Vasilev@x5.ru>
2021-09-14 02:03:23 +0300

    Admin_panel_sprint_2 filters fixed

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -2,6 +2,7 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 from django_filters import rest_framework as filters
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from api.v1.serializers import FilmWorkSerializer
 from movies.models import FilmWork, FilmWorkPersonRole
@@ -45,6 +46,6 @@ class FilmWorkViewSet(viewsets.ReadOnlyModelViewSet):
         .all()
     )
     serializer_class = FilmWorkSerializer
-    filter_backends = (filters.filters.SearchFilter, filters.DjangoFilterBackend)
+    filter_backends = (SearchFilter, filters.DjangoFilterBackend)
     search_fields = ('title', 'description')
     filterset_class = FilmWorkFilter
