802b332dbb90610bed604e7200f18150e0e2f33b
Viktor.Vasilev <Viktor.Vasilev@x5.ru>
2021-09-14 01:29:29 +0300

    Admin_panel_sprint_2 fix pagination

--- movies_admin/api/pagination.py
+++ movies_admin/api/pagination.py
@@ -1,15 +1,21 @@
 from collections import OrderedDict
 
+from django.core.paginator import EmptyPage
 from rest_framework import pagination
 from rest_framework.response import Response
 
 
 class PrimitivePagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
+        try:
+            next_page_number = self.page.next_page_number()
+        except EmptyPage:
+            next_page_number = None
+
         return Response(OrderedDict([
             ('count', self.page.paginator.count),
             ('total_pages', self.page.paginator.num_pages),
-            ('prev', self.page.previous_page_number() if self.page.has_previous() else 1),
-            ('next', self.page.next_page_number()),
+            ('prev', self.page.previous_page_number() if self.page.has_previous() else None),
+            ('next', next_page_number),
             ('results', data)
         ]))
