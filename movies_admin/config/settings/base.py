564aa9241a3ca4efb37ef670a96174ff60a5bc85
Viktor.Vasilev <Viktor.Vasilev@x5.ru>
2021-09-19 02:57:09 +0300

    Admin_panel_sprint_2 comments after review done

--- movies_admin/config/settings/base.py
+++ movies_admin/config/settings/base.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
